4f60b4f7e180c25bf637f9aff717b8af4d828e9f
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-12 19:05:40 +0100

    updated task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Calculate the total number of non-unique fans for each country origin and rank them
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
