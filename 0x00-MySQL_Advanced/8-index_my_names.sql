15d33e535deadce3d17204ce0dba9f9e8c6d8ab3
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-12 20:02:45 +0100

    updated task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
 -- Create an index idx_name_first on the first letter of the name column in the names table
-CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
+CREATE INDEX idx_name_first ON names(name(1));
