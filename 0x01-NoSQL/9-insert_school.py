9742a62359e9dbf425d767d96df9aa7c93b452bf
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-17 19:36:07 +0100

    updated task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,10 +9,12 @@ def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document into a MongoDB collection.
 
-    :param mongo_collection: The MongoDB collection where the document will be inserted.
+    :param mongo_collection: The MongoDB collection
+    where the document will be inserted.
     :type mongo_collection: pymongo.collection.Collection
 
-    :param kwargs: Key-value pairs representing the fields and values for the new document.
+    :param kwargs: Key-value pairs representing the fields
+    and values for the new document.
     :type kwargs: dict
 
     :return: The ObjectID of the newly inserted document.
