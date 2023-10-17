539641d70aba9b22af01588788090d7de2ec9223
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-17 19:45:43 +0100

    updated task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Change all topics of a school document
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics for a school in a MongoDB collection.
+
+    :param mongo_collection: The MongoDB collection where
+    the document will be updated.
+    :type mongo_collection: pymongo.collection.Collection
+
+    :param name: The name of the school to update.
+    :type name: str
+
+    :param topics: The new topics to set for the school.
+    :type topics: list
+
+    :return: A pymongo.results.UpdateResult object representing
+    the result of the update operation.
+    :rtype: pymongo.results.UpdateResult
+    """
+    return mongo_collection.updateMany(
+        {
+            "name": name
+        },
+        {
+            "$set": {
+                "topics": topics
+               }
+        }
+     )
