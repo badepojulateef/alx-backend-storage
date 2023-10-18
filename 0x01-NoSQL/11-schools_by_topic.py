4d82a91a6638e98d892dde2479acfad4e519dde4
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-18 03:59:27 +0100

    added task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+List of schools having a specific topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Find schools in a MongoDB collection that have a
+    specific topic among their topics.
+
+    :param mongo_collection: The MongoDB collection to search for schools.
+    :type mongo_collection: pymongo.collection.Collection
+
+    :param topic: The topic to search for among school topics.
+    :type topic: str
+
+    :return: A cursor pointing to the documents that match the specified topic.
+    :rtype: pymongo.cursor.Cursor
+    """
+    return mongo_collection.find({"topics": topic})
